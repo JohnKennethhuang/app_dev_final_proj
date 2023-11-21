fc2faa1666ae444cc31a8f351b486d3335c23e3f
l3m0n42 <Alberto.G.Yerena@gmail.com>
2023-11-20 16:51:29 -0800

    idk man I just work here

--- l3mon.py
+++ l3mon.py
@@ -1,3 +1,42 @@
 # Alberto's code 
 
-print("Hello Alberto")
+# client’s name, burger’s name, time of the day, and the total bill
+
+# Name of the client number 10
+# The client with the longest name
+# Top three most selling burgers (based on the number of burgers)
+# Top three best clients (highest bills)
+# Name of the client with the second-to-last lowest bill
+# Busiest hour of the day (number of clients)
+# Best hour of the day (sales)
+# Total sales on the day
+
+class Client:
+    def __init__(self, name, burger, time, total):
+        self.name = name
+        self.burger = burger
+        self.time = time
+        self.total = total
+
+def burger_selection():
+    print("1.cheeseburger, 2.hamburger, 3.double_double, 4.big mac, 5.baconator")
+    burger = input('What kind of burger would you like: ')
+    while burger not in ('1','2','3','4','5'):
+        burger = input("Invalid selection, please try again\n")
+    return burger
+
+def main():
+    customers = []
+    for i in range(3):
+        name = input('Customer name: ')
+        burger = burger_selection()
+        time = 12
+        total = 13
+
+
+    customer = Client(name, burger, time, total)
+    customers.append(customer)
+    # loop to check if shit is working 
+    # for customer in customers:
+    #         print(f"Name: {customer.name}, Burger: {customer.burger}, Time: {customer.time}, Total: {customer.total}")
+main()
