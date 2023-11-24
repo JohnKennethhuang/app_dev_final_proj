9c23603e6610028ad774d131d049cb5cc1cad47a
John Kenneth Huang <huangjohnkenneth@gmail.com>
2023-11-23 16:28:13 -0800

    changed the burger_selection func to allow customers to add multiple items to their order and confirm when they are finished

--- john.py
+++ john.py
@@ -1,4 +1,5 @@
-# John's code
+# John's Code 
+
 
 # client’s name, burger’s name, time of the day, and the total bill
 
@@ -11,6 +12,10 @@
 # Best hour of the day (sales)
 # Total sales on the day
 
+# No more than 30 clients per day 
+# 5 types of borgers 
+# 11:00 am to 10:00 pm 
+
 class Client:
     def __init__(self, name, burger, time, total):
         self.name = name
@@ -18,12 +23,26 @@ class Client:
         self.time = time
         self.total = total
 
+# Function changed to allow customers to add multiple items to their order
+# and to confirm if they are finished and if not they can add more         
+ 
 def burger_selection():
     print("1.cheeseburger, 2.hamburger, 3.double_double, 4.big mac, 5.baconator")
-    burger = input('What kind of burger would you like: ')
-    while burger not in ('1','2','3','4','5'):
-        burger = input("Invalid selection, please try again\n")
-    return burger
+    selections = [] 
+
+    while True:
+        burger = input("Enter the number of the burger you would like to add to your order\nONE NUMBER AT A TIME: ")
+
+        if burger in ("1", "2", "3", "4", "5",):
+            selections.append(burger)
+        else:
+            print("Invalid selection, please try again.\n")
+        
+        more = input("Would you like to add another burger ot your order? (yes/no): ")
+        if more.lower() != "yes":
+            break 
+
+    return selections 
 
 def main():
     customers = []
